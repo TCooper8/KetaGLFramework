83fa2402e986a543bcf91d76d349ac95d57265c7
Tyler Cooper <tcooper8@dmail.dixie.edu>
2013-04-07 00:00:40 -0600

    I'm no longer a commit virgin! :D
    Yay.

---
+++ KetaGLFramework/KetaGLFramework/Game.h
@@ -0,0 +1,18 @@
+namespace KetaFramework
+{
+#ifndef Game_h
+#define Game_h
+
+	class Game
+	{
+	private:
+		bool isActive;
+		//LaunchParameters parameters;
+		//GameWindow window;
+
+	public:
+		Game();
+	};
+
+#endif
+}
---
+++ KetaGLFramework/KetaGLFramework/LaunchParameters.h
@@ -0,0 +1,16 @@
+namespace KetaGraphics
+{
+#ifndef LaunchParameters_h
+#define LaunchParameters_h
+
+	class LaunchParameters
+	{
+	public:
+		unsigned int DisplayMode;
+		unsigned int MatrixMode;
+		int BackBufferWidth;
+		int BackBufferHeight;
+	};
+
+#endif
+}
